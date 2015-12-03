ea86c38967af1fa542176932ad302cbaf8676038
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2015-12-03 12:56:51 +0000

    completed matching parens solution

--- matching_parens.py
+++ matching_parens.py
@@ -18,16 +18,43 @@ import unittest
 def find_closing_paren(instr, start):
     """given the start paren, find its closing paren.
     """
-    return 0
+    if instr[start] != '(':
+        raise KeyError
+
+    stack = 1
+    for idx, letter in enumerate(instr[start+1:]):
+        if stack == 0:
+            return idx + start
+        if letter == '(':
+            stack += 1
+        elif letter == ')':
+            stack -= 1
+
+    if stack == 0:
+        return idx + start + 1
+    return -1
 
 
 class Tests(unittest.TestCase):
 
     def test_one(self):
+        s = "()"
+        self.assertEqual(
+            find_closing_paren(s, 0), 1)
+
+        s = "01(34)67"
+        self.assertEqual(
+            find_closing_paren(s, 2), 5)
+
         s = ("Sometimes (when I nest them (my parentheticals) too much"
              "(like this (and this))) they get confusing.")
         self.assertEqual(
-            find_closing_paren(s, 10), 79)
+            find_closing_paren(s, 10), 78)
+
+    def test_closing_not_found(self):
+        s = "here a paren: ( that doesn't close"
+        self.assertEqual(
+            find_closing_paren(s, 14), -1)
 
 
 if __name__ == '__main__':
